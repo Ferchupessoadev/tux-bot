e44af0e923ab9d812d0cdbd371041a2b2d333264
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2026-02-13 22:24:43 -0300

    hotfix: bug with cron_job

--- src/tasks.py
+++ src/tasks.py
@@ -9,14 +9,25 @@ def setup_tasks(bot, config):
 
     @tasks.loop(seconds=60)
     async def cron_job_youtube():
-        link = latest_video_link(bot, config)
-
         channel = bot.get_channel(int(config["CHANNEL_ID_YOUTUBE_DISCORD"]))
-        if channel and link != bot.latest_video_link:
+        if not channel:
+            return
+
+        link = latest_video_link(bot, config)
+        if not link:
+            return
+
+        if bot.latest_video_link is None:
             bot.latest_video_link = link
-            await channel.send(f"""
-            @here\n**¡Hey!, Hay algo Nuevo en el Canal de YouTube.  No te lo pierdas!**\n{link}
-            """)
+            return
+
+        if link != bot.latest_video_link:
+            bot.latest_video_link = link
+            await channel.send(
+                "@everyone\n"
+                "**¡Hey! 🔥 Hay algo nuevo en el canal de YouTube. ¡No te lo pierdas!**\n"
+                f"{link}"
+            )
 
     @tasks.loop(minutes=5)
     async def update_stats():
